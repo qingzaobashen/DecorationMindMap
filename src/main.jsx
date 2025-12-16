9bc6cf32debbecc98a988dfd8caab4c4e8997fb0
Your Name <you@example.com>
2025-12-16 22:51:39 +0800

    refactor(路由): 将BrowserRouter替换为HashRouter以支持静态部署

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom';
 import { UserProvider } from './context/UserContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <UserProvider>
       <App />
       </UserProvider>
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 )
